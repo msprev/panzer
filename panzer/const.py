568e7ba95ee6d29e0e43fece526791f146073903
Mark Sprevak <mark.sprevak@ed.ac.uk>
2014-09-02 11:45:58 +0100

    refactor const.py

--- panzer/const.py
+++ panzer/const.py
@@ -1,13 +1,27 @@
+""" constants for panzer code """
 import os
 
 REQUIRE_PANDOC_ATLEAST = "1.12.1"
+
 DEFAULT_SUPPORT_DIR = os.path.join(os.path.expanduser('~'), '.panzer')
+
 ENCODING = 'utf8'
+
+# keys to access type and content of metadata fields
 T = 't'
 C = 'c'
-ADDITIVE_FIELDS = ['preflight',
-                   'filter',
-                   'postprocess',
-                   'postflight',
-                   'cleanup']
 
+RUNLIST_KIND = ['preflight',
+                'filter',
+                'postprocess',
+                'postflight',
+                'cleanup']
+
+# status of items on runlist
+QUEUED = 'queued'
+RUNNING = 'running'
+FAILED = 'failed'
+DONE = 'done'
+
+# ast of an empty pandoc document
+EMPTY_DOCUMENT = [{'unMeta': {}}, []]
