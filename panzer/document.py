78c8ddfa680559962ff493847618b61807c11c37
Mark Sprevak <sprevak.home@gmail.com>
2016-10-31 10:09:41 +0000

    fix https://github.com/msprev/panzer/issues/25#issuecomment-257186272

--- panzer/document.py
+++ panzer/document.py
@@ -45,7 +45,7 @@ class Document(object):
                 'stdin_temp_file' : str()
             },
             'pandoc': {
-                'input'      : list(),
+                'input'      : ['-'],
                 'output'     : '-',
                 'pdf_output' : False,
                 'read'       : str(),
